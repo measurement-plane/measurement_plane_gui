4522e85fdf935d6496f6ad1005efd25b6ed96b5f
Abderrahim <62219710+amlabdr@users.noreply.github.com>
2025-01-18 13:18:29 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set environment variables
+CONTAINER_NAME="measurement_plane_gui_container"
+IMAGE_NAME="amlabdr/measurement_plane_gui:latest"
+BROKER_URL=${BROKER_URL:-"amqp://localhost:5672/"}
+
+# Stop and remove any running container with the same name
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+# Pull the latest pre-built image
+docker pull $IMAGE_NAME
+
+# Run the container
+docker run --name "$CONTAINER_NAME" \
+    -e BROKER_URL="$BROKER_URL" \
+    -p 8050:8050 \
+    "$IMAGE_NAME" || {
+    echo "Error: Failed to start the container."
+    exit 1
+}
