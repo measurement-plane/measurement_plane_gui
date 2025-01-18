453e93262c4c00283c936caa31423fcc3fd47611
Abderrahim <62219710+amlabdr@users.noreply.github.com>
2025-01-18 13:59:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ IMAGE_NAME="amlabdr/measurement_plane_gui:latest"
 BROKER_URL=${BROKER_URL:-"amqp://localhost:5672/"}
 
 # Stop and remove any running container with the same name
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+docker stop $CONTAINER_NAME >/dev/null 2>&1 || true
+docker rm $CONTAINER_NAME >/dev/null 2>&1 || true
 
 # Pull the latest pre-built image
 docker pull $IMAGE_NAME
